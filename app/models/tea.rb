ca8b9a5832acad881c668e4dc6ed74fdf58f5560
Garrett Gregor <garrett.gregor@gmail.com>
2023-09-18 15:48:44 -0600

    feat: add tea model

---
+++ app/models/tea.rb
@@ -0,0 +1,11 @@
+class Tea < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :temperature, presence: true, numericality: true
+  validates :brew_time, presence: true, numericality: true
+
+  has_many :subscription_teas
+  has_many :subscriptions, through: :subscription_teas
+  has_many :customer_subscriptions, through: :subscriptions
+  has_many :customers, through: :customer_subscriptions
+end
