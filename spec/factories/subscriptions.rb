f69f0fafc7e9691506e2006910cfb8174cb5ece8
Garrett Gregor <garrett.gregor@gmail.com>
2023-09-18 17:08:36 -0600

    update tea factory faker data

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :subscription do
-    title { Faker::Name.first_name}
-    price { Faker::Name.last_name}
-    status { Faker::Number.between(from: 0, to: 1)}
-    frequency { Faker::Number.between(from: 1, to: 120)}
+    title { Faker::Subscription.plan }
+    price { Faker::Name.last_name }
+    status { Faker::Number.between(from: 0.0, to: 100.0) }
+    frequency { Faker::Number.between(from: 1, to: 120) }
   end
 end
--- spec/factories/teas.rb
+++ spec/factories/teas.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :tea do
-    title { Faker::Name.first_name}
-    description { Faker::Lorem.last_name}
+    title { Faker::Tea.variety}
+    description { Faker::Lorem.sentence}
     temperature { Faker::Number.between(from: 170, to: 212)}
     brew_time { Faker::Number.between(from: 150, to: 300)}
   end
