e95dbd2a415e451ddea4b871b2c56621493e6d55
Garrett Gregor <garrett.gregor@gmail.com>
2023-09-19 15:25:09 -0600

    add index for email

---
+++ db/migrate/20230919211547_add_unique_index_to_customers.rb
@@ -0,0 +1,5 @@
+class AddUniqueIndexToCustomers < ActiveRecord::Migration[7.0]
+  def change
+    add_index :customers, :email, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_19_033916) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_19_211547) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_19_033916) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_customers_on_email", unique: true
   end
 
   create_table "subscriptions", force: :cascade do |t|
