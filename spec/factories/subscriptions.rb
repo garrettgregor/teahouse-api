017060af7764180e00f26664d9bd8b817f447bd8
Garrett Gregor <garrett.gregor@gmail.com>
2023-09-18 18:39:47 -0600

    update faker number

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :subscription do
     title { Faker::Subscription.plan }
-    price { Faker::Number.between(from: 0.0, to: 100.0) }
+    price { Faker::Number.decimal(l_digits: 1, r_digits: 2) }
     status { Faker::Number.between(from: 0, to: 1) }
     frequency { Faker::Number.between(from: 1, to: 120) }
   end
